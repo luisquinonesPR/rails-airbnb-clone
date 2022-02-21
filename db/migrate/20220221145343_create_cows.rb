d6d76836bec24dd7f151eb5bbf0e5abd4bea3e32
Luis Quinones <luis.quinones9@upr.edu>
2022-02-21 16:06:00 +0100

    controllers and get rid of images

--- db/migrate/20220221145343_create_cows.rb
+++ db/migrate/20220221145343_create_cows.rb
@@ -3,7 +3,6 @@ class CreateCows < ActiveRecord::Migration[6.1]
     create_table :cows do |t|
       t.string :name
       t.text :description
-      t.string :image
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
