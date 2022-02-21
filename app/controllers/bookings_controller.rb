b59a7161aa46c37628e74d446ef297660d1fc064
Luis Quinones <luis.quinones9@upr.edu>
2022-02-21 21:14:31 +0100

    part of seeding and controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_cow, only: [ :new, :create ]
+
+  def index
+    @bookings = Booking.all.where(user_id: current_user.id)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.cow = @cow
+    @booking.user = current_user
+    if @booking.save
+      redirect_to cow_bookings_path(@cow)
+    else
+      redirect_to cow_path(@cow)
+    end
+  end
+
+  private
+
+  def set_cow
+    @cow = Cow.find(params[:cow_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -1,2 +1,35 @@
 class CowsController < ApplicationController
+  before_action :set_cow, only: [:show, :edit, :update, :destroy]
+  def index
+    @cows = Cow.all
+  end
+
+  def create
+    @cow = Cow.new(cow_params)
+    @cow.user = current_user
+    if @cow.save
+      redirect_to cow_path(@cow)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cow.destroy
+    redirect_to root_path
+  end
+
+  def my_cows
+    @cows = current_user.cows
+  end
+
+  private
+
+  def set_cow
+    @cow = Cow.find(params[:id])
+  end
+
+  def cow_params
+    params.require(:cow).permit(:name, :description, :photo)
+  end
 end
