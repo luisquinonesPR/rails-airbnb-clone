aae66230879f4fb5ca43438a3bc44ddb9df48cdf
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-21 17:04:28 +0100

    git to continue

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :cow
+  has_many :cows
+  has_many :users
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :cow, uniqueness: { scope: :start_date }
+  validates :cow, uniqueness: { scope: :end_date }
 end
--- app/models/cow.rb
+++ app/models/cow.rb
@@ -1,3 +1,5 @@
 class Cow < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
 end
