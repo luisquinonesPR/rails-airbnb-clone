983c3a5a6c3f128be0707081ab938e206aababd3
Luis Quinones <luis.quinones9@upr.edu>
2022-02-23 11:24:40 +0100

    sent received bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :cow
-  has_many :cows
-  has_many :users
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :cow, uniqueness: { scope: :start_date }
