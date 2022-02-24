18113eecbe7a870a1f20f59343f98e4b920a0a39
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-24 14:57:10 +0100

    modified gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,8 @@ photos = ['https://images.unsplash.com/photo-1527153857715-3908f2bae5e8?ixlib=rb
   'https://images.unsplash.com/photo-1556997685-309989c1aa82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80'
 ]
 
+
+
 10.times do
   cow = Cow.create!(
     name:Faker::TvShows::GameOfThrones.character,
