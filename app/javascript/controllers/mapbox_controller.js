8cd56f76c9c64a76447dbd76d416de879d1b4393
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-24 21:10:36 +0100

    modified mapbox controller to add cow icon

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -17,13 +17,23 @@ export default class extends Controller {
 // call the methods
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
+    this.addMarkersToMap();
   }
  // add markers to the map, the # refers that it is a private method
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window) // add this
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div")
+      customMarker.className = "marker"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = "contain"
+      customMarker.style.width = "25px"
+      customMarker.style.height = "25px"
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map)
     });
@@ -34,4 +44,5 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 })
   }
+
 }
