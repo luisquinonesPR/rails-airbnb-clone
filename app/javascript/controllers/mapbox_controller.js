beeb1a08af956d7a57835f67d363b275348aa95e
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-24 20:34:05 +0100

    added animation to the map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -3,7 +3,7 @@ import mapboxgl from "mapbox-gl"
 
 export default class extends Controller {
   static values = {
-    apiKey: String,
+    apiKey: String, // is the token is a string
     markers: Array
   }
 
@@ -14,17 +14,17 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-
+// call the methods
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
   }
-  // [ ... ]
+ // add markers to the map, the # refers that it is a private method
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window) // add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // add this
+        .setPopup(popup)
         .addTo(this.map)
     });
   }
@@ -32,9 +32,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 })
   }
 }
-
-
-// app/javascript/controllers/mapbox_controller.js
