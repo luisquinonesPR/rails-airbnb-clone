32769fd61b3e758f0691a57c946f99263f3dc16f
lidia illanes <l.illanes.roel@gmail.com>
2022-02-25 08:13:19 +0100

    small chages

--- app/models/cow.rb
+++ app/models/cow.rb
@@ -4,6 +4,6 @@ class Cow < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   has_one_attached :photo
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
